42db81fbba8b069e5c4bba8227457ce4aba95a9c
longyunbo <longyunbo@guohuaigroup.com>
2018-07-25 14:51:15 +0800

    first commit

---
+++ src/main/java/com/drag/cstgroup/CstgroupApplication.java
@@ -0,0 +1,12 @@
+package com.drag.cstgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CstgroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CstgroupApplication.class, args);
+	}
+}
